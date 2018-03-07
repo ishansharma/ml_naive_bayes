499ce3a057576b440f5d97b3e7d280fe14532645
Ishan <contact@ishansharma.com>
2018-03-07 01:47:18 -0600

    Refactoring file reader to make testing easier

--- file_reader/file_reader.py
+++ file_reader/file_reader.py
@@ -5,6 +5,8 @@ import re
 
 
 class FileReader:
+    index = 0
+
     def __init__(self, dir_path):
         """
         Initialise with path.
@@ -20,9 +22,10 @@ class FileReader:
         try:
             dir_path = os.path.abspath(dir_path)  # converting directory to a path
             if os.path.isdir(dir_path):
-                files = list(filter(self.filter_text_files, os.listdir(dir_path)))  # list of files in the directory
-                self.document_count = len(files)
-                self.vocabulary = self.construct_vocabulary(dir_path, files)
+                self.files = list(map(lambda file_path: (os.path.join(dir_path, file_path)),
+                                      filter(self.filter_text_files, os.listdir(dir_path))))
+                self.document_count = len(self.files)
+                self.vocabulary = self.construct_vocabulary(self.files)
             else:
                 tb = sys.exc_info()[2]
                 raise FileNotFoundError().with_traceback(tb)
@@ -30,38 +33,61 @@ class FileReader:
             print("File not found exception", f)
             exit(0)
 
-    def construct_vocabulary(self, dir_path, files):
+    def construct_vocabulary(self, files):
         """
         Given a list of files, read them one by one and construct vocabulary
+
         Parameters
         ----------
         files
+
+        Returns
+        -------
+        vocab : dict
         """
         vocab = {}
 
         for file in files:
-            # temporary hack to avoid file
-            if file == "2248.2004-09-23.GP.spam.txt":
-                continue
-
-            with io.open(os.path.join(dir_path, file), 'r', encoding='utf8') as f:
-                # replace new lines with spaces
-                file_text = f.read().replace('\n', ' ')
-
-                # strip everything but text and numbers
-                file_text = re.sub(r'[\W+]', ' ', file_text)
-
-                # remove excess spaces
-                file_text = re.sub(r'\s+', ' ', file_text)
-
-                file_text = file_text.split(' ')
-
-                for word in file_text:
-                    if word in vocab:
-                        vocab[word] += 1
-                    else:
-                        vocab[word] = 1
+            words = self.get_word_frequency(file)
+            for word in words:
+                if word in vocab:
+                    vocab[word] += words[word]
+                else:
+                    vocab[word] = words[word]
         return vocab
+
+    def get_word_frequency(self, file_path):
+        """
+        Accept a file_path and return the list of words along with frequency for it
+
+        Parameters
+        ----------
+        file_path
+
+        Returns
+        -------
+        words : dict
+        """
+        words = {}
+
+        with io.open(file_path, 'r', encoding='utf8', errors='ignore') as f:
+            # replace new lines with spaces
+            file_text = f.read().replace('\n', ' ')
+
+            # strip everything but text and numbers
+            file_text = re.sub(r'[\W+]', ' ', file_text)
+
+            # remove excess spaces
+            file_text = re.sub(r'\s+', ' ', file_text)
+
+            file_text = file_text.split(' ')
+
+            for word in file_text:
+                if word in words:
+                    words[word] += 1
+                else:
+                    words[word] = 1
+        return words
 
     def filter_text_files(self, file_path):
         """
