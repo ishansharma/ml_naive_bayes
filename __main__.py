177f2001f359ee35caad392b9a2d2e4139ecc57d
Ishan <contact@ishansharma.com>
2018-03-06 22:27:13 -0600

    Constructing vocabulary

--- __main__.py
+++ __main__.py
@@ -1,8 +1,8 @@
 import argparse
+from file_reader import file_reader
 
 parser = argparse.ArgumentParser(description='Run Naive Bayes and Logistic Regression on given dataset and report '
                                              'accuracy')
-
 # arguments
 #  ham_directory
 #  spam_directory
@@ -10,10 +10,19 @@ parser = argparse.ArgumentParser(description='Run Naive Bayes and Logistic Regre
 #  limit_iterations
 #  filter_stop_words
 
-parser.add_argument('ham_directory', type=str, help="Path to directory containing all the ham")
-parser.add_argument('spam_directory', type=str, help="Path to directory containing all the spam")
-parser.add_argument('lambda', type=int, help="Regularization parameter for logistic regression")
-parser.add_argument('limit_iterations', type=int, help="Hard limit on number of iterations for logistic regression")
-parser.add_argument('filter_stop_words', type=str, help="Whether to filter stop words or not")
+parser.add_argument('--ham_directory', type=str, help="Path to directory containing all the ham")
+parser.add_argument('--spam_directory', type=str, help="Path to directory containing all the spam")
+parser.add_argument('--lambda', type=int, help="Regularization parameter for logistic regression")
+parser.add_argument('--limit_iterations', type=int, help="Hard limit on number of iterations for logistic regression")
+parser.add_argument('--filter_stop_words', type=str, help="Whether to filter stop words or not")
 
 args = parser.parse_args()
+
+# read ham
+ham = file_reader.FileReader(args.ham_directory)
+print(ham.document_count, "ham files found")
+
+# read spam
+spam = file_reader.FileReader(args.spam_directory)
+print(spam.document_count, "spam files found")
+
---
+++ file_reader/file_reader.py
@@ -0,0 +1,83 @@
+import os.path
+import sys
+import io
+import re
+
+
+class FileReader:
+    document_count = 0
+    vocabulary = {}
+
+    def __init__(self, dir_path):
+        """
+        Initialise with path.
+        If path is not a directory or does not contain txt files, raise relevant exception and exit.
+
+        Otherwise, count number of files and construct vocabulary
+
+        Parameters
+        ----------
+        dir_path: str
+            String containing path to ham or spam files
+        """
+        try:
+            dir_path = os.path.abspath(dir_path)  # converting directory to a path
+            if os.path.isdir(dir_path):
+                files = list(filter(self.filter_text_files, os.listdir(dir_path)))  # list of files in the directory
+                self.document_count = len(files)
+                self.construct_vocabulary(dir_path, files)
+            else:
+                tb = sys.exc_info()[2]
+                raise FileNotFoundError().with_traceback(tb)
+        except FileNotFoundError as f:
+            print("File not found exception", f)
+            exit(0)
+
+    def construct_vocabulary(self, dir_path, files):
+        """
+        Given a list of files, read them one by one and construct vocabulary
+        Parameters
+        ----------
+        files
+        """
+        for file in files:
+            # temporary hack to avoid file
+            if file == "2248.2004-09-23.GP.spam.txt":
+                continue
+
+            with io.open(os.path.join(dir_path, file), 'r', encoding='utf8') as f:
+                # replace new lines with spaces
+                file_text = f.read().replace('\n', ' ')
+
+                # strip everything but text and numbers
+                file_text = re.sub(r'[\W+]', ' ', file_text)
+
+                # remove excess spaces
+                file_text = re.sub(r'\s+', ' ', file_text)
+
+                file_text = file_text.split(' ')
+
+                for word in file_text:
+                    if word in self.vocabulary:
+                        self.vocabulary[word] += 1
+                    else:
+                        self.vocabulary[word] = 1
+
+    def filter_text_files(self, file_path):
+        """
+        If given file path is of type txt, return true
+
+        Parameters
+        ----------
+        file_path
+
+        Returns
+        -------
+        bool
+        """
+        file = os.path.splitext(file_path)
+
+        if file[1] == ".txt":
+            return True
+
+        return False
